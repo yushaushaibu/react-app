1aa383b93b006142aeed41c4370d016591d0181a
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-01 10:20:15 +0000

    Passing multiple props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import CommentDetail from "./CommentDetail";
+import { faker } from "@faker-js/faker";
 
 const App = () => {
   return (
     <div>
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Kim" />
-      <CommentDetail author="Mike" />
+      <CommentDetail author="Alex" image={faker.image.avatar()} time="Today at 9:45AM" content="How artistic!" />
+      <CommentDetail author="Kim" image={faker.image.avatar()} time="Today at 5:03AM" content="Nice blog post!" />
+      <CommentDetail author="Mike" image={faker.image.avatar()} time="Yesterday at 11:23PM" content="Great work :)" />
     </div>
   );
 };
--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { faker } from "@faker-js/faker";
 
 const CommentDetail = (props) => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
+          <img alt="avatar" src={props.image} />
         </a>
         <div className="content">
           <a href="/" className="author">
             {props.author}
           </a>
           <div className="metadata">
-            <span className="date">Today at 19:34</span>
+            <span className="date">{props.time}</span>
           </div>
-          <div className="text">How artistic!</div>
+          <div className="text">{props.content}</div>
         </div>
       </div>
     </div>
